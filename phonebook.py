9482de4474bfb7f2c1a418af4829a44796e85a88
Vbifus-spb <gb@medvedev.in>
2022-09-13 13:33:27 +0300

    edit records

--- phonebook.py
+++ phonebook.py
@@ -34,7 +34,7 @@ def find_abon():
     flag = ''
     lname = input('Введите фамилию или ее часть для поиска \n или Enter для выхода ')
     flag = lname
-    cur.execute("SELECT * FROM phonebook WHERE mylower(lname) LIKE ? or mylower(fname) LIKE  ? or phone LIKE ?;", ("%" + lname + "%", "%" + lname + "%", "%" + lname + "%",))
+    cur.execute("SELECT rowid, fname, lname, phone FROM phonebook WHERE mylower(lname) LIKE ? or mylower(fname) LIKE  ? or phone LIKE ?;", ("%" + lname + "%", "%" + lname + "%", "%" + lname + "%",))
     all_results = cur.fetchall()
     print('\n')
     if flag == '':
@@ -44,16 +44,76 @@ def find_abon():
         print('Данные отсутствуют. Уточните условия поиска ')
     else:
         for q in all_results:
-            print(' '.join(q), '\n')
-            
-exim.im
+            q1 = q[0]
+            q2 = q[1:]
+            print(q1, ' '.join(q2), '\n')
 
 
+
+
+def update_abon():
+    find_abon()
+    r = input('Для выбора измененяемой записи введите соответсвующую цифру слева  если передумали - Enter ')
+    if r != '':
+        r_id = int(r)
+        cur.execute("SELECT * FROM phonebook WHERE  rowid = ?;", ( r_id , ))
+        result = cur.fetchall()
+        for q in result:
+            print('\n', ' '.join(q))
+        while True:
+            flag = input('Меняем? Y/N ')
+            if flag == 'y' or flag == 'Y':
+                lname = input('Введите фамилию или ENTER, если не меняем ')
+                if lname == '':
+                    lname = q[1]
+                fname = input('Введите имя или ENTER, если не меняем ')
+                if fname == '':
+                    fname = q[0]
+                phone = input('Введите номер телефона или ENTER, если не меняем ')
+                if phone == '':
+                    phone = q[2]
+                #print(fname)
+                cur.execute("UPDATE phonebook SET fname = ?,lname = ?, phone = ? WHERE rowid = ?;", (fname, lname, phone, r_id))
+                conn.commit()
+                print('Записано')
+                break
+            elif flag == 'n' or flag == 'N':
+                break
+
+def delet_abon():
+    find_abon()
+    r = input('Для выбора удаляемой записи введите соответсвующую цифру слева  если передумали - Enter ')
+    if r != '':
+        r_id = int(r)
+        cur.execute("SELECT * FROM phonebook WHERE  rowid = ?;", ( r_id , ))
+        result = cur.fetchall()
+        for q in result:
+            print('\n', ' '.join(q))
+        while True:
+            flag = input('Меняем? Y/N ')
+            if flag == 'y' or flag == 'Y':
+                cur.execute("DELETE FROM users WHERE rowid = ?;", (r_id,))
+                conn.commit()
+                print('Удалено')
+                break
+            elif flag == 'n' or flag == 'N':
+                break
+
+
+
+    #print(result)
+
+
+#find_abon()
+#choos_adon()
+update_abon()
+
+'''
 while True:
 
     if find_abon() == '':
         break
     else:
         find_abon
-
+'''
 conn.close()
